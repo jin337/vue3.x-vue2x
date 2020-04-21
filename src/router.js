ae81528862954fd4ec69285d47218231949b4ced
You <you9009@foxmail.com>
2020-04-21 16:48:52 +0800

    init

--- src/router.js
+++ src/router.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHashHistory } from 'vue-router'
+import { createRouter, createWebHistory } from 'vue-router'
 
 const routes = [
   {
@@ -19,7 +19,7 @@ const routes = [
 ]
 
 const router = createRouter({
-  hash: createWebHashHistory(process.env.BASE_URL),
+  history: createWebHistory(process.env.BASE_URL),
   routes
 })
 
