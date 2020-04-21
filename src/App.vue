84711ca630577fa119fd7d73d33ed9fe73cb4e6b
You <you9009@foxmail.com>
2020-04-21 16:51:14 +0800

    init

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,9 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/" exact>Home</router-link> |
-      <router-link to="/about" exact>About</router-link> |
-      <router-link to="/test" exact>Test</router-link>
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link> |
+      <router-link to="/test">Test</router-link>
     </div>
     <router-view/>
   </div>
