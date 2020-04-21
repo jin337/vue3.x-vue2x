97ba0b77be2ba9fbeb44c449409e3facbe44eb05
You <you9009@foxmail.com>
2020-04-21 17:03:20 +0800

    init

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,6 +1,6 @@
 <template>
   <h2>路由名：{{route}}</h2>
-  <h1>vue2x-data：{{number}}</h1>
+  <h1>vue3-ref：{{number}}</h1>
   <h2>computed：当前数据的两倍：{{doubleNum}}</h2>
   <p><button @click="add">增加</button></p>
   <h2>获取Vuex中的值：{{testNum}}</h2>
@@ -8,38 +8,40 @@
 </template>
 
 <script>
-import { mapState, mapMutations } from 'vuex'
+import { ref, watch, computed, getCurrentInstance } from 'vue'
 export default {
-  name: 'Home',
-  data () {
+  name: 'About',
+  setup () {
+    const { ctx } = getCurrentInstance()
+    // 初始化属性
+    const number = ref(12)
+    const route = ctx.$router.currentRoute.value.name
+    // 事件绑定
+    const add = () => { number.value++ }
+    const update = () => { ctx.$store.commit('setTest', number) }
+
+    // 计算属性
+    const doubleNum = computed(() => number.value * 2)
+    const testNum = computed(() => ctx.$store.state.test)
+
+    // 数据监听
+    watch(
+      number, () => { console.log(`监控number数据为${number.value}`) }, { lazy: true }
+    )
+    watch(
+      doubleNum, () => { console.log(`监控doubleNum数据为${doubleNum.value}`) }
+    )
+
+    // 获取路由
+
     return {
-      number: 12
+      route,
+      number,
+      doubleNum,
+      testNum,
+      add,
+      update
     }
-  },
-  created () {
-    this.route = this.$route.name
-  },
-  computed: { // 计算属性
-    ...mapState([
-      'test'
-    ]),
-    testNum () {
-      return this.test
-    },
-    doubleNum () {
-      return this.number * 2
-    }
-  },
-  methods: { // 事件绑定
-    ...mapMutations([
-      'setTest'
-    ]),
-    add () { this.number++ },
-    update () { this.setTest(this.number) }
-  },
-  watch: {
-    number: (val) => { console.log(`监控number数据为${val}`) },
-    doubleNum: (val) => { console.log(`监控doubleNum数据为${val}`) }
   }
 }
 </script>
