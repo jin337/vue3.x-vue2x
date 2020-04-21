fe77b0b8b5509d4d35a2cfb299bd30b6bdb055d7
You <you9009@foxmail.com>
2020-04-21 17:35:35 +0800

    init

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,5 @@
 <template>
-  <!-- <h2>路由名：{{route}}</h2> -->
+  <h2>路由名：{{routeName}}</h2>
   <h1>vue3-ref：{{number}}</h1>
   <h2>computed：当前数据的两倍：{{doubleNum}}</h2>
   <p><button @click="add">增加</button></p>
@@ -8,14 +8,13 @@
 </template>
 
 <script>
-import { ref, watch, computed, getCurrentInstance, onMounted } from 'vue'
+import { ref, watch, computed, getCurrentInstance } from 'vue'
 export default {
   name: 'About',
   setup () {
     const { ctx } = getCurrentInstance()
     // 初始化属性
     const number = ref(12)
-    // const route = ctx.$router.currentRoute.value.name
     // 事件绑定
     const add = () => { number.value++ }
     const update = () => { ctx.$store.commit('setTest', number) }
@@ -23,6 +22,7 @@ export default {
     // 计算属性
     const doubleNum = computed(() => number.value * 2)
     const testNum = computed(() => ctx.$store.state.test)
+    const routeName = computed(() => ctx.$router.currentRoute.value.name)
 
     // 数据监听
     watch(
@@ -32,14 +32,10 @@ export default {
       doubleNum, () => { console.log(`监控doubleNum数据为${doubleNum.value}`) }
     )
 
-    onMounted(() => {
-      console.log(ctx)
-    })
-
     // 获取路由
 
     return {
-      // route,
+      routeName,
       number,
       doubleNum,
       testNum,
