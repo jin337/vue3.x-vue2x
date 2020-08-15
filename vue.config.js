f42cfccc7d37c5bfc88ef4efb9b2fae684155d21
You <you9009@foxmail.com>
2020-08-15 14:07:10 +0800

    init

--- vue.config.js
+++ vue.config.js
@@ -1,42 +1,24 @@
 const path = require('path')
-const CompressionPlugin = require('compression-webpack-plugin')
 
 module.exports = {
-  outputDir: 'dist',
-  publicPath: '/vue3.0-demo/dist/',
-  productionSourceMap: false,
-  runtimeCompiler: true,
-  assetsDir: 'assets',
-  devServer: {
-    host: 'localhost',
-    port: 1111,
-    open: true,
-    inline: true,
-    hot: true,
-    stats: 'minimal',
-    contentBase: __dirname,
-    overlay: true,
-    historyApiFallback: true
-  },
-  configureWebpack: {
-    resolve: {
-      alias: {
-        src: path.join(__dirname, 'src'),
-        views: path.join(__dirname, 'src/views'),
-        store: path.join(__dirname, 'src/store'),
-        assets: path.join(__dirname, 'src/assets'),
-        components: path.join(__dirname, 'src/components')
-      }
-    },
-    plugins: [
-      new CompressionPlugin({
-        filename: '[path].gz[query]',
-        algorithm: 'gzip',
-        test: new RegExp('\\.(js|css)$'),
-        threshold: 10240,
-        minRatio: 0.8,
-        deleteOriginalAssets: false
-      })
-    ]
-  }
+	outputDir: 'dist',
+	productionSourceMap: false,
+	runtimeCompiler: true,
+	assetsDir: 'assets',
+	devServer: {
+		host: 'localhost',
+		port: 1111,
+		open: true
+	},
+	configureWebpack: {
+		resolve: {
+			alias: {
+				src: path.join(__dirname, 'src'),
+				views: path.join(__dirname, 'src/views'),
+				store: path.join(__dirname, 'src/store'),
+				assets: path.join(__dirname, 'src/assets'),
+				components: path.join(__dirname, 'src/components')
+			}
+		}
+	}
 }
